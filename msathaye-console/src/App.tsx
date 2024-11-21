8f5d66a1ef9ceb366088d737e9eddfb4ddfd8989
maanas <sathayemaanas@gmail.com>
2024-11-21 17:11:17 -0600

    init frontend

---
+++ msathaye-console/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="blog-container">
+      <h1>Why Hello There</h1>
+    </div>
+  )
+}
+
+export default App
